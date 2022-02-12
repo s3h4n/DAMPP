1a2521c18d1caccaa0315d54f2fc02b59a63a65b
s3h4n <shehanrweerasekara@gmail.com>
2022-02-12 13:46:33 +0530

    Initial Release

--- __main__.py
+++ __main__.py
@@ -1,7 +1,20 @@
+"""
+    Name       : DAMPP
+    Version    : 1.0
+    Description: DAMPP is a program that allows you to create dockerized
+                 containers that are able to run simple Apache Php web servers.
+    Author     : Sehan Weerasekara (s3h4n)
+    Date       : 12-02-2022
+    Contact    : https://github.com/s3h4n
+"""
+
 from .src import Handler
 
 
-def main():
+def main() -> None:
+    """
+    main is the entry point for the program.
+    """
     Handler().handler()
 
 
