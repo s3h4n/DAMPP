dd95331d74b25f5fc71f9d76616800ee97a0fb9e
s3h4n <shehanrweerasekara@gmail.com>
2022-02-12 12:41:19 +0530

    Initial Release

--- packages/frontend/dialogs/quick_dialog.py
+++ packages/frontend/dialogs/quick_dialog.py
@@ -2,12 +2,40 @@ from PyQt6 import QtWidgets
 
 
 class Error(object):
+    """
+    Error will show an error message.
+
+    :param object: self
+    :type object: object
+    """
+
     def show(self, message: str) -> None:
+        """
+        show will show an error message.
+
+        :param message: The message to show.
+        :type message: str
+        """
         QtWidgets.QMessageBox().critical(None, "Error", message)
 
 
 class Confirm(object):
-    def show(self, message: str) -> None:
+    """
+    Confirm will show a confirmation message.
+
+    :param object: self
+    :type object: object
+    """
+
+    def show(self, message: str) -> bool:
+        """
+        show will show a confirmation message.
+
+        :param message: The message to show.
+        :type message: str
+        :return: True if the user confirms. False otherwise.
+        :rtype: bool
+        """
         result = QtWidgets.QMessageBox.question(
             None,
             "Confirm",
@@ -15,6 +43,7 @@ class Confirm(object):
             QtWidgets.QMessageBox.StandardButton.Yes,
             QtWidgets.QMessageBox.StandardButton.No,
         )
+
         if result == QtWidgets.QMessageBox.StandardButton.Yes:
             return True
         else:
